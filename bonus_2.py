951c50b98c37efab4e50f83fcc4933ef2b998c8c
Turki Ahmed <ttaiealmutairi@stu.kau.edu.sa>
2025-06-18 13:00:18 +0300

    Turki Ahmed

--- bonus_2.py
+++ bonus_2.py
@@ -3,11 +3,10 @@
 name:str = input("Enter your name: ")
 email:str = input("Enter your email: ")
 
-
 if len(name) <= 2:
     print("the name length must be more than 2 characters, please provide a valid name.")
 else:    
-    if email.find("@gmail") > 0 and len(email) > (len("@gmail") + 2):
+    if email.find("@gmail.") > 2 and email.count("@gmail.") == 1 and not email.startswith("@") and not email.find(" ") > -1 and (email.find("@gmail.") + 8) < len(email):
         print(f"welcome {name}, you registered with the email {email} !")
     else:
         print("the email is not valid , please provide a valid email .")
